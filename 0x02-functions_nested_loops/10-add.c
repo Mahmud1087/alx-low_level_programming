97f6b6663c7230a479fdeea6ff96f4e7b04d334a
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-07-05 19:16:52 -0700

    all tasks completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * add - this function adds two integers and returns the result
+ *
+ * @a: number one
+ * @b: number two
+ *
+ * Return: Addition of number one and two
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
