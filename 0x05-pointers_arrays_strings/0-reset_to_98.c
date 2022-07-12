d87e7a15c41ce9834b905a8bd52e1ce20610fcdf
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-07-12 05:50:16 -0700

    Created a new directory, a readme file, main.h header file and 2 tasks files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - Function that takes a pointer to an integer
+* @n: puntero
+* Return: (0)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README file for projects on  pointers, arrays and strings in C
