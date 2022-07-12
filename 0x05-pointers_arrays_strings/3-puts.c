2f53fdfdaacd93a29863222c50fd48093f449c7c
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-07-12 06:11:37 -0700

    added task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts - This prints a string, to standard output
+ *@str: value to be evaluated.
+ *Return: 0.
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*(str + l) != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+	return 0;
+}
